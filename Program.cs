173d8c3d182d7b26c0bd601e2e62c90b799746b1
Tiago Sabath <tiago.sabath@gmail.com>
2023-11-03 11:50:27 -0300

    Desafio Concluído !!

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Iphone: ");
+Smartphone iphone = new Iphone("1234","IPhone 14","111111",64);
+iphone.Ligar();
+iphone.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Nokia: ");
+Smartphone nokia = new Nokia("9876","Nokia Tijolão","5555",128);
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Whatsapp");
